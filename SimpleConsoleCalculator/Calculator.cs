8d3edd7d622db1b8f174294decac54a9e1a92413
Maxim <maks96rush@yandex.ru>
2021-09-01 19:01:01 +0300

    init: starting the project

---
+++ SimpleConsoleCalculator/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleCalculator
+{
+    public class Calculator
+    {
+        public double Calculate(double firstValue, string operation, double secondValue)
+        {
+            if (operation == "+")
+            {
+                return firstValue + secondValue;
+            }
+            if (operation == "-")
+            {
+                return firstValue - secondValue;
+            }
+            if (operation == "*")
+            {
+                return firstValue * secondValue;
+            }
+            if (operation == "/")
+            {
+                if (secondValue == 0)
+                {
+                    throw new DivideByZeroException("An error has been occured: Can not divide by zero!");
+                }
+                return firstValue / secondValue;
+            }
+            else
+            {
+                Console.WriteLine("An error has been occured: inapplicable operator");
+                Console.WriteLine("Please enter one of these operators: '+', '-', '*', '/'");
+
+                operation = Console.ReadLine();
+
+                if (operation == "+")
+                {
+                    return firstValue + secondValue;
+                }
+                if (operation == "-")
+                {
+                    return firstValue - secondValue;
+                }
+                if (operation == "*")
+                {
+                    return firstValue * secondValue;
+                }
+                if (operation == "/")
+                {
+                    if (secondValue == 0)
+                    {
+                        throw new DivideByZeroException("An error has been occured: Can not divide by zero!");
+                    }
+                    return firstValue / secondValue;
+                }
+            }
+            return firstValue;
+        }
+    }
+}
---
+++ SimpleConsoleCalculator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace SimpleConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture
+                = new System.Globalization.CultureInfo("en-US");
+
+            Calculator calculator = new Calculator();
+
+            while (true)
+            {
+                Console.Clear();
+
+                try
+                {
+                    while (true)
+                    {
+                        double firstValue;
+                        double secondValue;
+                        double result;
+
+                        string operation;
+                        string input;
+                        string[] splitted;
+
+                        input = Console.ReadLine();
+                        splitted = input.Split(" ");
+
+                        firstValue = double.Parse(splitted[0]);
+                        operation = splitted[1];
+                        secondValue = double.Parse(splitted[2]);
+
+                        result = calculator.Calculate(firstValue, operation, secondValue);
+
+                        Console.Write(result + " ");
+
+                        while (true)
+                        {
+                            splitted = Console.ReadLine().Split(" ");
+
+                            firstValue = result;
+                            operation = splitted[0];
+                            secondValue = double.Parse(splitted[1]);
+
+                            result = calculator.Calculate(firstValue, operation, secondValue);
+                            Console.Write(result + " ");
+                        }
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("An error has been occured: Can not divide by zero!");
+                    Console.WriteLine("Press \"enter\" to refresh");
+                    Console.ReadLine();
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number");
+                    Console.WriteLine("Press \"enter\" to refresh");
+                    Console.ReadLine();
+                    continue;
+                }
+            }
+        }
+    }
+}
